a4e7094f51465f27f9e5a83c4ff5e31f1bf539b9
unknown <arfinchowdhuryarif7@gmail.com>
2022-12-10 12:25:14 +0600

    alhamdullillah all major problem and also completed

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var jwt = require('jsonwebtoken');
 const express = require("express");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+const ObjectID = require('mongodb').ObjectId;
+const stripe = require("stripe")(process.env.SCREAT_key);
+
+// console.log(process.env.SCREAT_key)
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,6 +19,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Wellcome to doctor's portal server");
@@ -49,23 +55,32 @@ const run = async() =>{
     try{
         await client.connect();
         const appointmentsCollection = client.db("doctorsportal2").collection("appointments2");
+        const paymentsCollection = client.db("doctorsportal2").collection("payments");
         const usersCollection = client.db("doctorsportal2").collection("users");
         const doctorsCollection = client.db("doctorsportal2").collection("doctors");
         // console.log('DB connected successfully.')
 
         app.get("/appointment",async(req,res)=>{
             const email = req.query.email;
+            
             const authorization = req.headers.authorization;
             // console.log("from appointment", authorization)
-            const date = new Date(req.query.date).toLocaleDateString();
+            // const date = new Date(req.query.date).toLocaleDateString();
             // console.log(date)
-            const query = { email: email, date:date};
+            const query = { email: email};
             // console.log(query);
             const cursor = appointmentsCollection.find(query);
             const appointments = await cursor.toArray();
             res.json(appointments);
         })
+        app.get('/booking/:id', async(req,res)=>{
+            const id = req.params;
+            const query = {_id: ObjectId(id)};
+            // console.log("Query----------",query)
+            const booking = await appointmentsCollection.findOne(query);
+            res.send(booking);
 
+        })
         app.get('/users',async(req,res)=>{
             const users = await usersCollection.find().toArray();
             res.send(users)
@@ -75,7 +90,7 @@ const run = async() =>{
             const email = req.params.email;
             const query = {email: email};
             const user = await usersCollection.findOne(query);
-            console.log(user)
+            // console.log(user)
             let isAdmin = false;
             if(user?.role === 'admin'){
                 isAdmin = true
@@ -83,12 +98,31 @@ const run = async() =>{
             res.send({admin: isAdmin})
         } )
 
+        app.post('/create-payment-intent', async(req,res)=>{
+            const service = req.body;
+            const price  = Number(service.price.split("$")[1]);
+            if(typeof(price) === "number"){
+                const amount = Number(price * 100);
+                const paymentIntent = await stripe.paymentIntents.create({
+                    amount : amount,
+                    currency: 'usd',
+                    automatic_payment_methods: {
+                        enabled: true,
+                      },
+                })
+                res.send({clientSecret : paymentIntent.client_secret})
+            }else{
+                console.log('Price is not a number')
+            }
+        })
 
         app.post("/appointment", async(req,res)=>{
             // console.log('enter here');
             // console.log(req.body);
             const appointmentInformation = req.body;
+
             const result = await appointmentsCollection.insertOne(appointmentInformation)
+            
             .then(result=>{
                 // console.log(`A document was inserted with the _id: ${result.insertedId}`);
                 res.json(result);
@@ -118,7 +152,7 @@ const run = async() =>{
 
         app.put('/users/admin',async(req,res)=>{
             const user =  req.body;
-            console.log("put-------", req.headers)
+            // console.log("put-------", req.headers)
             const filter = {email: user.email};
             
             const updateDoc = {$set:{role: 'admin'}};
@@ -126,14 +160,41 @@ const run = async() =>{
             res.json(result);
 
         })
+       
+        app.patch('/booking/:id', async(req,res)=>{
+            const id = req.params.id;
+            const payment = req.body;
+            // console.log("Payment----------", payment, id)
+            const  filter  = {_id : ObjectID(id)}
+                            
+            const updateDoc = {
+                $set:{
+                    paid : true,
+                    transactionId: payment.transitionId,
+                }
+            }
+            const result = await paymentsCollection.insertOne(payment);
+            const updatedBooking = await appointmentsCollection.updateOne(filter, updateDoc);
+            res.send(updatedBooking)
+
+        })
+
         app.get('/doctor',async(req,res)=>{
             const doctors = await doctorsCollection.find().toArray();
             res.send(doctors);
         })
         app.post('/doctor',async(req,res)=>{
             const doctor = req.body;
-            console.log(doctor)
+            // console.log(doctor)
             const result = await doctorsCollection.insertOne(doctor);
+            res.send(result);
+        })
+        app.delete('/doctor/:id',async(req,res)=>{
+            const id = req.params.id;
+            // console.log(id)
+            const filter = {_id: ObjectID(id)}
+            const result = await doctorsCollection.deleteOne(filter);
+            // console.log(result)
             res.send(result);
         })
 
